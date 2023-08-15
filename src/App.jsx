7798483acfbe2f40a09c862079e73e00f8a49d73
ornob90 <oornob49@gmail.com>
2023-08-15 16:11:35 +0600

    destination section with responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import Destinations from "./components/Destinations";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import viteLogo from "/vite.svg";
@@ -9,6 +10,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
+      <Destinations />
     </>
   );
 }
